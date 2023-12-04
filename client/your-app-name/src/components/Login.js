605a90bc95fac8d05ae72843f210d004f10c78a6
junmospecial <113631384+junmospecial@users.noreply.github.com>
2023-12-03 23:52:47 -0500

    login page

---
+++ client/your-app-name/src/components/Login.js
@@ -0,0 +1,68 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import './Login.css';
+
+function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState(''); // State for the error message
+  const navigate = useNavigate();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    setErrorMessage(''); // Clear any existing error messages
+
+    try {
+      const response = await axios.post('http://localhost:5000/users/signIn', {
+        email,
+        password,
+      });
+
+      if (response.data.status === "SUCCESS") {
+        // Set localStorage and navigate based on user role
+        localStorage.setItem('name', response.data.data.name);
+        localStorage.setItem('userId', response.data.data._id);
+        localStorage.setItem('email', response.data.data.email);
+        localStorage.setItem('isAdmin', response.data.data.isAdmin);
+
+        if (response.data.data.isAdmin) {
+          navigate('/AdminPage');
+        } else if (response.data.data.isDisabled) {
+          navigate('/DisabledPage');
+        } else {
+          navigate('/herofinder');
+        }
+      } else {
+        setErrorMessage(response.data.message || 'Login failed');
+      }
+    } catch (error) {
+      setErrorMessage('Error during login: ' + error.message);
+    }
+  };
+
+  const navigateToSignUp = () => {
+    navigate('/signup');
+  };
+
+  const navigateBack = () => {
+    navigate('/');
+  };
+
+  return (
+    <div className="login-container">
+        <form onSubmit={handleSubmit} className="login-form">
+            <h1>Login</h1>
+            {errorMessage && <p className="login-error">{errorMessage}</p>}
+            <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email" required />
+            <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" required />
+            <button type="submit">Login</button>
+            <p className="signup-text">Don't have an account? Sign up here!</p>
+            <button onClick={navigateToSignUp} className="signup-button">Sign Up</button>
+            <button onClick={navigateBack} className="back-button">Back</button>
+        </form>
+    </div>
+  );
+}
+
+export default Login;
