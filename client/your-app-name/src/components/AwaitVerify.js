c485e13be3a524c74c17dc5de15c692179f36826
junmospecial <113631384+junmospecial@users.noreply.github.com>
2023-12-03 23:53:17 -0500

    await verify

---
+++ client/your-app-name/src/components/AwaitVerify.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './AwaitVerify.css'; // Ensure this path is correct
+
+function AwaitVerify() {
+    return (
+        <div className="await-verify-container">
+            <h1>Success!</h1>
+            <p>Check your email inbox to log in and enjoy the experience!</p>
+        </div>
+    );
+}
+
+export default AwaitVerify;
