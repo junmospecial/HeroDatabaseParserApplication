cf83643dd18613411043e8c451f24958c20697f8
junmospecial <113631384+junmospecial@users.noreply.github.com>
2023-12-03 23:53:03 -0500

    email verification client

---
+++ client/your-app-name/src/components/EmailVerification.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function EmailVerification() {
+  return (
+    <div>
+      <h1>Email Verification</h1>
+      <p>Please check your email to verify your account.</p>
+    </div>
+  );
+}
+
+export default EmailVerification;
